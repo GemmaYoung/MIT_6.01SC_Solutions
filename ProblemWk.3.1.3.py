02525511e07af0ff0d00af67766d4d463b00fbe1
Yuan Yun <yuanyun@expample.com>
2019-09-12 16:59:14 +0800

    Using sm class default methods

--- ProblemWk.3.1.3.py
+++ ProblemWk.3.1.3.py
@@ -3,9 +3,8 @@ from lib601 import sm
 class PureFunction(sm.SM):
     def __init__(self, f):
         self.f = f
-        self.startState = None
-    def getNextValues(self, state, inp):
-        return (state, self.f(inp))
+    def getNextState(self, state, inp):
+        return self.f(inp)
 
 s = PureFunction(float)
 print s.transduce([3,5])
