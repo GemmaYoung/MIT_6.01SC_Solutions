77fe5d2e156071d28887d343eea0ca5c1706b2e9
Yuan Yun <yuanyun@expample.com>
2019-12-07 16:32:52 +0800

    solution for Problem Wk.7.2.5 Argopt

---
+++ ProblemWk.7.2.5.py
@@ -0,0 +1,24 @@
+import operator
+
+def argmin(f, input):
+    bestValSoFar = None
+    bestArgSoFar = None
+    for x in input:
+        if bestValSoFar == None or f(x) < bestValSoFar:
+            bestValSoFar = f(x)
+            bestArgSoFar = x
+    return (bestValSoFar, bestArgSoFar)
+
+## print  argmin(lambda x: (x-3)**2, [1,2,3,4])
+
+def argopt(f, input, comp):
+    bestValSoFar = None
+    bestArgSoFar = None
+    for x in input:
+        if bestValSoFar == None or comp(f(x), bestValSoFar):
+            bestValSoFar = f(x)
+            bestArgSoFar = x
+    return (bestValSoFar, bestArgSoFar)
+
+print argopt(lambda x: (x-3)**2, [1,2,3,4], operator.lt)
+print argopt(lambda x: x[0], [(1,2),(3,4)], operator.lt)
